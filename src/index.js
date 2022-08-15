f318412645a942463387f42459ce0fed10fce15e
gavpin <gavinpinto@me.com>
2022-08-15 12:11:08 +0800

    Initial Commit from VScode

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { FronteggProvider } from '@frontegg/react';
+
+const contextOptions = {
+  baseUrl: 'https://app-jx0f956ls8rp.frontegg.com',
+  clientId: '57cf8733-8da2-4b33-b436-882928a6ec49'
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>>
     <App />
-  </React.StrictMode>
+  </FronteggProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
